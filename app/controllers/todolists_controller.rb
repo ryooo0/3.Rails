1a45e9d8a8fc9caa9290478ccf126c20680d2fd4
EC2 Default User <ec2-user@ip-172-31-91-1.ec2.internal>
2021-10-09 02:22:55 +0000

    [Finish]2-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,14 +4,18 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
-  # 以下を追加
   def create
-    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   private
